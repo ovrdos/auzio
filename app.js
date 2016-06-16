e61c327c28c59dc19f7f8dfaba510f92d88b18c4
Kamal Hakim <kamalhakim@livenation.com>
2016-06-16 11:55:41 -0700

    Updating nodejs.

--- app.js
+++ app.js
@@ -1,39 +1,23 @@
 var port = process.env.PORT || 3000,
     http = require('http'),
     fs = require('fs'),
+    express = require('express'),
+    app = express(),
     html = fs.readFileSync('index.html');
 
 var log = function(entry) {
     fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
 };
 
-var server = http.createServer(function (req, res) {
-    if (req.method === 'POST') {
-        var body = '';
+var oneDay = 86400000;
 
-        req.on('data', function(chunk) {
-            body += chunk;
-        });
+//app.use(express.compress());
 
-        req.on('end', function() {
-            if (req.url === '/') {
-                log('Received message: ' + body);
-            } else if (req.url = '/scheduled') {
-                log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
-            }
+app.use(express.static(__dirname + '/', { maxAge: oneDay }));
+app.use(express.static(__dirname + '/js', { maxAge: oneDay }));
+app.use(express.static(__dirname + '/media', { maxAge: oneDay }));
 
-            res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
-            res.end();
-        });
-    } else {
-        res.writeHead(200);
-        res.write(html);
-        res.end();
-    }
-});
-
-// Listen on port 3000, IP defaults to 127.0.0.1
-server.listen(port);
+app.listen(port);
 
 // Put a friendly message on the terminal
 console.log('Server running at http://127.0.0.1:' + port + '/');
