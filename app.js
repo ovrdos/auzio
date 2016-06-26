c253cf461c31331e5b470aa1a283830628f94608
Kamal Hakim <kamalhakim@livenation.com>
2016-06-26 00:08:25 -0700

    Adding auzio update. fixing app.js

--- app.js
+++ app.js
@@ -1,27 +1,18 @@
-var port = process.env.PORT || 5555,
+var port = process.env.PORT || 3000,
     fs = require('fs'),
-    https = require('https'),
     express = require('express'),
     app = express();
-
-var privateKey = fs.readFileSync('cert/client-key.pem');
-var certificate = fs.readFileSync('cert/cert.pem');
 
 var log = function(entry) {
     fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
 };
 
 var oneDay = 86400000;
+
 app.use(express.static(__dirname + '/', { maxAge: oneDay }));
 app.use(express.static(__dirname + '/js', { maxAge: oneDay }));
 app.use(express.static(__dirname + '/media', { maxAge: oneDay }));
 
-//https.createServer({
-//    key: privateKey,
-//    cert: certificate
-//}, app).listen(port);
+app.listen(port);
 
-app.listen(3000);
-
-console.log('Server running at https://127.0.0.1:' + port + '/');
-console.log('Server running at http://127.0.0.1:' + 3000 + '/');
+console.log('Server running at http://127.0.0.1:' + port + '/');
